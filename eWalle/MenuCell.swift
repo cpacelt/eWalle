c8d18f1f14f1367395433641f4f71f8bc4021129
Roman Putintsev <rptsev@gmail.com>
2021-10-28 00:27:14 +0300

    Added menu table view

---
+++ eWalle/MenuCell.swift
@@ -0,0 +1,61 @@
+//
+//  MenuCell.swift
+//  eWalle
+//
+//  Created by Роман Путинцев on 27.10.2021.
+//
+
+import UIKit
+
+class MenuCell: UITableViewCell {
+    // MARK: - Reuse identifier
+    static let reuseIdentifier = "MenuCell"
+    
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var cellBookmarkView: UIView!
+    @IBOutlet weak var cellLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+
+        prepareToShow()
+        setupConstraints()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: false)
+        
+        if selected {
+            cellBookmarkView.backgroundColor = self.tintColor
+            //Need to becom global an enum
+            cellLabel.font = UIFont.boldSystemFont(ofSize: 16)
+            cellLabel.sizeToFit()
+        } else {
+            cellBookmarkView.backgroundColor = self.backgroundColor
+            cellLabel.font = UIFont.systemFont(ofSize: 16)
+        }
+        
+    }
+    
+    // MARK: Prepare for showing
+    func prepareToShow() {
+        self.selectionStyle = .none
+    }
+    
+    //MARK: - Constraints setup
+    func setupConstraints() {
+        cellBookmarkView.translatesAutoresizingMaskIntoConstraints = false
+        
+        cellBookmarkView.leftAnchor.constraint(equalTo: self.contentView.leftAnchor).isActive = true
+        cellBookmarkView.topAnchor.constraint(equalTo: self.contentView.topAnchor).isActive = true
+        cellBookmarkView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor).isActive = true
+        cellBookmarkView.rightAnchor.constraint(equalTo: self.contentView.leftAnchor, constant: 2).isActive = true
+    }
+    
+
+}
+
+
--- eWalle/MenuViewController.swift
+++ eWalle/MenuViewController.swift
@@ -8,22 +8,80 @@
 import UIKit
 
 class MenuViewController: UIViewController {
-
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var accountBackgroundRoundedView: UIView!
+    @IBOutlet weak var accountStackView: UIStackView!
+    @IBOutlet weak var nameLocationStackView: UIStackView!
+    
+    //Account views
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    //Menu table
+    @IBOutlet weak var menuTableView: UITableView!
+    
+    //MARK: - LifeCircle methods
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        prepareSubviews()
+        constraintsSetup()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    //MARK: - Subviews preparation
+    func prepareSubviews(){
+        // Account view
+        accountBackgroundRoundedView.layer.cornerRadius = 50
+        accountBackgroundRoundedView.clipsToBounds = true
+        
+        // Menu table view
+        menuTableView.dataSource = self
+        menuTableView.delegate = self
     }
-    */
+    
+    //MARK: - Actions
+    
+    //MARK: - Constraints setup method
+    func constraintsSetup() {
+        
+        accountBackgroundRoundedView.translatesAutoresizingMaskIntoConstraints = false
+        accountStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        accountBackgroundRoundedView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: accountBackgroundRoundedView.layer.cornerRadius * -1.0).isActive = true
+        accountBackgroundRoundedView.topAnchor.constraint(equalTo: view.topAnchor, constant: accountBackgroundRoundedView.layer.cornerRadius * -1.0).isActive = true
+        accountBackgroundRoundedView.rightAnchor.constraint(equalTo: view.centerXAnchor, constant: 30).isActive = true
+        accountBackgroundRoundedView.bottomAnchor.constraint(equalTo: view.topAnchor, constant: accountBackgroundRoundedView.layer.cornerRadius * 1.0 * 2.5).isActive = true
+        
+        accountStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
+        accountStackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        accountStackView.rightAnchor.constraint(equalTo: accountBackgroundRoundedView.rightAnchor, constant: -20).isActive = true
+        accountStackView.bottomAnchor.constraint(equalTo: accountBackgroundRoundedView.bottomAnchor, constant: -20).isActive = true
+    }
+
+}
+
+
+// MARK: - menuTableView data source
+extension MenuViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+}
+
+extension MenuViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = menuTableView.dequeueReusableCell(withIdentifier: MenuCell.reuseIdentifier) as! MenuCell
+        
+        return cell
+    }
+    
 
 }
